9646aedeaec88012e297b6d4ea0a167f7c705ade
Minahil-Areej <minahil.areej24@gmail.com>
2024-12-04 19:00:14 +0000

    Fix: Import 'path' module for serving static files

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path'); // Import the 'path' module
 const userRoutes = require('./routes/userRoutes');
 const portfolioRoutes = require('./routes/portfolioRoutes');
 const jwt = require('jsonwebtoken');
